683f155228cef9974ad7e776e55c55a46b0d0e64
MuhaammadSyaban28 <syaban28@upi.edu>
2026-01-12 17:58:09 +0700

    Create Environment.java

---
+++ tomasgame/src/main/java/model/Environment.java
@@ -0,0 +1,33 @@
+package model;
+
+public class Environment {
+    private int minX;
+    private int maxX;
+    private int minY;
+    private int maxY;
+
+    // Constructor using the gamePane's dimensions
+    public Environment(double minX, double maxX, double minY, double maxY) {
+        this.minX = (int) minX;
+        this.maxX = (int) maxX;
+        this.minY = (int) minY;
+        this.maxY = (int) maxY;
+    }
+
+    // Getters
+    public int getMinX() {
+        return minX;
+    }
+
+    public int getMaxX() {
+        return maxX;
+    }
+
+    public int getMinY() {
+        return minY;
+    }
+
+    public int getMaxY() {
+        return maxY;
+    }
+}   
